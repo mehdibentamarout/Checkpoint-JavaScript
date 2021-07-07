0c56f2fb36e287966586ac516fc77af55e7ff6ec
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-07-07 20:36:31 +0200

    init

---
+++ WarmUp5.js
@@ -0,0 +1,24 @@
+// 1) Write a function named greaterNum that:
+// - takes 2 arguments, both numbers.
+// - returns whichever number is the greater (higher) number.
+// ex greaterNum(5, 10) => "The greater number of 5 and 10 is 10."
+
+function greaterNum() {
+  // TODO: your code here
+}
+
+// 2) Write a function named isEven using  for loop that
+// -  iterate from x to y.
+// -  return array contain the even values,
+//  ex:  isEven(1,10) => [2,4,6,8,10]
+
+function isEven() {
+  // TODO: your code here
+}
+
+//3) write a function named sum that
+// - Use a while loop to add up the numbers from x to y.
+// ex sum(1,5) => 15
+function sum() {
+  // TODO: your code here
+}
