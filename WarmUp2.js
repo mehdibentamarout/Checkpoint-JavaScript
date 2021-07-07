6ae0b0218d649bb2015f96d8eb4aad4f4ebcc474
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-07-07 20:36:10 +0200

    init

---
+++ WarmUp2.js
@@ -0,0 +1,23 @@
+// 1 - Write functions larger and smaller that each accept two strings as arguments,
+// and return the larger and smaller strings, respectively
+
+function Larger() {}
+
+// 2 - Write a function called counter that, when invoked, always returns a number that 
+// is one more than the previous invocation.
+// For instance:
+
+function counting() {
+  // TODO: your code here
+}
+
+// counting(5); // => '1, 2, 3, 4, 5'
+// counting(1); // => '1'
+// counting(3); // => '1, 2, 3'
+
+// 3- Write a function shortestWord that works like longestWord,
+// but returns the shortest word instead.
+
+function shortestWord() {
+  // TODO: your code here
+}
