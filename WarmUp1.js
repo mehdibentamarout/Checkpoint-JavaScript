573398d3d99bf2340361a6b85ffcb45ebda6e6f9
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-07-07 20:36:01 +0200

    init

---
+++ WarmUp1.js
@@ -0,0 +1,19 @@
+// 1-using + operator combine your partner first and last name .
+
+function combine() {
+  // TODO: your code here
+}
+
+// 2-find if the number 13 is a multiple of 3 or not.
+function MultipleOF3() {
+  // TODO: your code here
+}
+
+// 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
+var ages = [13, 14, 13, 15, 16, 17, 19, 13, 16, 15];
+
+// 4-calculate your age in seconds.
+
+function AgesInSeconds() {
+  // TODO: your code here
+}
