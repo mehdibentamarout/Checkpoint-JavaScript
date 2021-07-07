378221ea300f7a93ef45cb3945744b00e7fdbb89
Your NameNeirouzJbira <jbira.neirouz@gmail.com>
2021-07-07 20:36:26 +0200

    init

---
+++ WarmUp4.js
@@ -0,0 +1,20 @@
+// 1-write a function that returns the product of any number by 3.
+function product3() {
+  // TODO: your code here
+}
+
+// 2-complete the following function multiply to return the result of multiplying of x and y
+function multiply() {
+  // TODO: your code here
+}
+
+// 3-complete the following function that Convert Fahrenheit to Celsius if the module for converting is
+// 	(5/9) * (fahrenheit-32);
+function convert() {
+  // TODO: your code here
+}
+
+// 4-Define a function named "sqArea", and make it display square area if you knew the length of its side.
+function sqArea() {
+  // TODO: your code here
+}
