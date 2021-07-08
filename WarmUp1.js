7bd9b5c84d90d367e9f0f718615429901b09f013
mehdibentamarout <86725150+mehdibentamarout@users.noreply.github.com>
2021-07-08 10:00:32 +0100

    Update WarmUp1.js

--- WarmUp1.js
+++ WarmUp1.js
@@ -1,13 +1,18 @@
 // 1-using + operator combine your partner first and last name .
 
-function combine() {
-  // TODO: your code here
+function combine(str1, str2) {
+  return str1 +" "+ str2;
 }
 
 // 2-find if the number 13 is a multiple of 3 or not.
-function MultipleOF3() {
-  // TODO: your code here
-}
+const isMultipleOfThree = num => {
+  if (num === 0) return true;
+
+  if (num < 0) return false;
+
+  return isMultipleOfThree(num - 3);
+};
+
 
 // 3-calculate the average age of the follwing ages [13,14,13,15,16,17,19,13,16,15]
 var ages = [13, 14, 13, 15, 16, 17, 19, 13, 16, 15];
